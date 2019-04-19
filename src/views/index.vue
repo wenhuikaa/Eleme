a4c05e8fda82cab982b40efd0855ad04b46a175f
wenhuikaa <wenhui4001@gmail.com>
2019-04-19 16:16:04 +0800

    首页

---
+++ src/views/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="index">
+    <!-- index下有子路由，所以不要忘记<router-view> -->
+    <!-- 路由匹配到的组件将显示到这里 -->
+    <router-view></router-view>
+    <TabBar :data="tabbarData"/>
+  </div>
+</template>
+
+<script>
+import TabBar from "../components/TabBar";
+export default {
+  name: "index",
+  data() {
+    return {
+      tabbarData: [
+        { title: "首页", icon: "iconshouye", path: "/home" },
+        { title: "订单", icon: "icondingdan", path: "/order" },
+        { title: "我的", icon: "iconwode", path: "/me" }
+      ]
+    };
+  },
+  components: {
+    TabBar
+  }
+};
+</script>
+
+<style scoped>
+.index {
+  width: 100%;
+  /* 因为底部导航本来就有45px */
+  height: calc(100%-45px);
+}
+</style>
